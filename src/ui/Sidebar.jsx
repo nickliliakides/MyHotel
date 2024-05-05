5874e8a2e8a9de2297a0158431b43af9e7002b0b
Nick Liliakides <nick.liliakides@gmail.com>
2024-05-05 04:33:33 +0100

    favicon change

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -7,7 +7,6 @@ const StyledSidebar = styled.header`
   background-color: var(--color-grey-0);
   padding: 3.2rem 2.4rem;
   border-right: 1px solid var(--color-grey-100);
-
   grid-row: 1/ -1;
 `;
 
